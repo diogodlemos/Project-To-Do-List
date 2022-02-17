3c35569f91fb93d165762b42f188664e9f73164a
diogodlemos <diogodrummond@hotmail.com>
2022-02-17 11:38:30 -0300

    modify setup of cors

--- index.js
+++ index.js
@@ -13,17 +13,13 @@ const {
 const { middlewareError } = require('./utils/middlewareError');
 
 app.use(express.json());
+app.use(cors());
 
-const corsOptions = {
-  origin: 'http://localhost:3000',
-  methods: 'GET'
-};
-
-app.post('/task', cors(corsOptions), insertTaskController);
-app.get('/task', cors(corsOptions), getAllTasksController);
-app.get('/task/:id', cors(corsOptions), getTaskByIdController);
-app.delete('/task/:id', cors(corsOptions), deleteTaskByIdController);
-app.put('/task/:id', cors(corsOptions), updateTaskController);
+app.post('/task', insertTaskController);
+app.get('/task', getAllTasksController);
+app.get('/task/:id', getTaskByIdController);
+app.delete('/task/:id', deleteTaskByIdController);
+app.put('/task/:id', updateTaskController);
 
 app.use(middlewareError);
 
